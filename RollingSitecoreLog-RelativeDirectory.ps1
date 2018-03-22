d6460bbdc64da9e33fc0eed718d246ff0c93ce94
gstreza <gstreza@rightpoint.com>
2018-03-22 16:14:01 -0500

    Keeping the window at the top

--- RollingSitecoreLog-RelativeDirectory.ps1
+++ RollingSitecoreLog-RelativeDirectory.ps1
@@ -83,6 +83,25 @@ Function Get-LogColor {
     }
 }
  
+<# Affix the window on top of all others #>
+$signature = @'
+[DllImport("user32.dll")] 
+public static extern bool SetWindowPos( 
+    IntPtr hWnd, 
+    IntPtr hWndInsertAfter, 
+    int X, 
+    int Y, 
+    int cx, 
+    int cy, 
+    uint uFlags); 
+'@
+  
+$type = Add-Type -MemberDefinition $signature -Name SetWindowPosition -Namespace SetWindowPos -Using System.Text -PassThru
+
+$handle = (Get-Process -id $Global:PID).MainWindowHandle 
+$alwaysOnTop = New-Object -TypeName System.IntPtr -ArgumentList (-1) 
+$type::SetWindowPos($handle, $alwaysOnTop, 0, 0, 0, 0, 0x0003)
+
 
  Try {
 
